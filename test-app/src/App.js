b18c07253762615f496e860831bd9608183c4c1e
Denys Barabash <20-6544-1@plus4u.net>
2018-04-04 13:05:51 +0300

    bind react and redux

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,38 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from './MyComponent.js';
+import {store} from './index.js'
+import { connect } from 'react-redux';
 
 class App extends Component {
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      value: '',
-    };
-  }
-
-  myCallBack(dataFromChildren) {
-    this.setState({value: dataFromChildren});
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="container">
+        <button onClick={() => {
+          this.props.setName("Denis")
+        }}>Set Name</button>
+        <p>
+          {this.props.user.name}
         </p>
-        <p className="App-intro">
-          {this.state.value}
+        <button onClick={() => {
+          this.props.setResultValue(800)
+        }}>Set Result</button>
+        <p>
+          {this.props.math.result}
         </p>
-        <MyComponent callBackFromParent={this.myCallBack} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+      user: state.user,
+      math: state.math
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setName: (name) => {
+            dispatch({
+                type: "SET_NAME",
+                payload: name
+            });
+        },
+        setResultValue: (value) => {
+          dispatch({
+            type: "ADD",
+            payload: value
+          })
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
--- test-app/src/index.js
+++ test-app/src/index.js
@@ -1,8 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {render} from "react-dom";
+import React from "react";
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./App.js";
+
+const mathReducer = (state = {
+    result: 1,
+    lastValues: []
+}, action) => {
+    switch (action.type) {
+        case "ADD":
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
+        case "SUBTRACT":
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
+    }
+    return state;
+};
+
+const userReducer = (state = {
+    name: "Max",
+    age: 27
+}, action) => {
+    switch (action.type) {
+        case "SET_NAME":
+            state = {
+                ...state,
+                name: action.payload
+            };
+            break;
+        case "SET_AGE":
+            state = {
+                ...state,
+                age: action.payload
+            };
+            break;
+    }
+    return state;
+};
+
+const store = createStore(
+    combineReducers({math: mathReducer, user: userReducer})
+);
+
+store.subscribe(() => {
+    // console.log("Store updated!", store.getState());
+});
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    window.document.getElementById('root'));
